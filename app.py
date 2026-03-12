d7fcfa886ea129448985b4c7d47bb244f1d64200
Cody91430 <menuhugo@orange.fr>
2026-03-12 15:40:45 +0100

    last exam push

--- app.py
+++ app.py
@@ -246,4 +246,4 @@ def delete_task(task_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host="0.0.0.0", port=5000)
